bfde57523213c510653699eb63d83cd5e62245d9
Arawir <mmarciniak87@wp.pl>
2018-07-21 16:41:42 +0200

    Cardline is ready

--- cardline_widget/cardline_widget_app.pro
+++ cardline_widget/cardline_widget_app.pro
@@ -1,9 +1,9 @@
 QT += core widgets 
 requires(qtConfig(combobox))
 
-HEADERS       = card.hh panel.hh
+HEADERS       = card.hh panel.hh cardline.hh
                 
-SOURCES       = card.cpp panel.cpp main.cpp
+SOURCES       = card.cpp panel.cpp cardline.cpp main.cpp
 
 # install
 target.path = $$[QT_INSTALL_EXAMPLES]/network/fortuneclient
--- cardline_widget/main.cpp
+++ cardline_widget/main.cpp
@@ -2,6 +2,7 @@
 #include <QCoreApplication>
 #include "card.hh"
 #include "panel.hh"
+#include "cardline.hh"
 
 qint32 Card_type::free_cards=10;
 
@@ -12,10 +13,13 @@ int main(int argc, char *argv[]){
   Card_type_panel C1;
   Card_type_panel C2;
   Card_type Ctype;
+  Cardline Cline;
+  
   C1.Set_card_type(&Ctype);
   C2.Set_card_type(&Ctype);
-  
-  C1.show();
-  C2.show();
+
+  Cline.Add_panel(&C1);
+  Cline.Add_panel(&C2);
+  Cline.show();
   return app.exec();
 }
